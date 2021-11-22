4375fbd40378fe607f727cc642d241abca8c2f16
juanmaMTR <jtoscanoreyes.guadalupe@alumnado.fundacionloyola.net>
2021-11-22 10:10:13 +0100

    Commit Inicial

---
+++ operacionesbd.php
@@ -0,0 +1,16 @@
+<?php
+    class Operaciones{
+        public $conexion=null;
+        function __construct(){
+            $this->conexion=mysqli_connect('localhost','root','','examen1_b');
+        }
+        function error(){
+            //echo $this->conexion->errno;
+            return $this->conexion->errno;
+        }
+        function consultar($consulta){
+            return $this->conexion->query($consulta);
+        }
+        
+    }
+?>
